ce5f12b16f2277c94f04eddae09af096d7cdf066
Eman Abdelmohsen <eman.abdelmohsen@hotmail.com>
2019-04-15 01:51:30 +0200

    ui layout

---
+++ src/isolette_monitor/Isolette_monitor.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isolette_monitor;
+
+/**
+ *
+ * @author Rehab Abdelmohsen
+ */
+public class Isolette_monitor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frm_main frame = new frm_main();
+        frame.show();
+    }
+    
+}
